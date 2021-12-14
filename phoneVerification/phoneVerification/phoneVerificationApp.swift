09a56688cbf75ab934f718e286897814868a5591
Arnas Peciukonis <arnaspeciukonis@Arnass-MacBook-Air.local>
2021-12-14 09:47:23 +0200

    Initial project commit

---
+++ phoneVerification/phoneVerification/phoneVerificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  phoneVerificationApp.swift
+//  phoneVerification
+//
+//  Created by Arnas Peciukonis on 2021-12-14.
+//
+
+import SwiftUI
+
+@main
+struct phoneVerificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
